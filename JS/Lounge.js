bf9bad41729ab5c222d4f3e807c1c69911b1ec14
Aaron Dangc <adraging@gmail.com>
2024-01-13 18:01:32 -0800

    added cards and background music

--- JS/Lounge.js
+++ JS/Lounge.js
@@ -24,6 +24,13 @@ class ConfigureScene extends Phaser.Scene {
     preload() {
         // Webfont script
         this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+        // Lounge Background Music
+        this.load.audio('loungeMusic', '../Assets/gameAssets/sounds/jazzMusic.mp3');
+
+        // Lounge Background Image
+        this.load.image('loungeBackground', '../Assets/gameAssets/images/sky.png');
+
+
     }
     create() {
         WebFont.load({
@@ -47,34 +54,81 @@ class Lounge extends ConfigureScene {
         super('Lounge');
     }
     create(){
-        const loungeText = this.add.text(this.scale.width * .2,this.scale.height * .05,'LuxeLagoon Gaming Lounge',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
+        // Lounge Background
+        const loungeBackground = this.add.sprite(0,0,'loungeBackground')
+        loungeBackground.setOrigin(0,0);
+        loungeBackground.setScale(.8);
+
+
+
+        // Lounge Background Music
+        const loungeMusic = this.sound.add('loungeMusic');
+        loungeMusic.setLoop(true);
+        loungeMusic.play();
+        const loungeText = this.add.text(this.scale.width * .04,this.scale.height * .05,'LuxeLagoon Gaming Lounge',{fontFamily: 'Ultra',fontSize: '6em',color: 'white'});
 
         // Roulette Button
-        const rouletteButton = this.add.text(this.scale.width * .2,this.scale.height * .2,'Roulette',{fontFamily: 'Ultra',fontSize: '2em',color: 'white'});
+        const rouletteButton = this.add.text(this.scale.width * .2,this.scale.height * .2,'Roulette',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
         rouletteButton.setInteractive();
         rouletteButton.on('pointerdown', () => {
+            loungeMusic.stop();
             this.scene.start('Roulette');
         });
 
+        rouletteButton.on('pointerover', () => {
+            rouletteButton.setColor('red');
+        });
+
+        rouletteButton.on('pointerout', () => {
+            rouletteButton.setColor('white');
+        });
+
         // Blackjack Button
-        const blackjackButton = this.add.text(this.scale.width * .2,this.scale.height * .3,'Blackjack',{fontFamily: 'Ultra',fontSize: '2em',color: 'white'});
+        const blackjackButton = this.add.text(this.scale.width * .2,this.scale.height * .3,'Blackjack',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
         blackjackButton.setInteractive();
         blackjackButton.on('pointerdown', () => {
+            loungeMusic.stop();
             this.scene.start('Blackjack');
         });
 
+        blackjackButton.on('pointerover', () => {
+            blackjackButton.setColor('red');
+        });
+
+        blackjackButton.on('pointerout', () => {
+            blackjackButton.setColor('white');
+        });
+
         // Slots Button
-        const slotsButton = this.add.text(this.scale.width * .2,this.scale.height * .4,'Slots',{fontFamily: 'Ultra',fontSize: '2em',color: 'white'});
+        const slotsButton = this.add.text(this.scale.width * .2,this.scale.height * .4,'Slots',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
         slotsButton.setInteractive();
         slotsButton.on('pointerdown', () => {
+            loungeMusic.stop();
             this.scene.start('Slots');
         });
 
+        slotsButton.on('pointerover', () => {
+            slotsButton.setColor('red');
+        });
+
+        slotsButton.on('pointerout', () => {
+            slotsButton.setColor('white');
+        });
+
         // Poker Button
-        const pokerButton = this.add.text(this.scale.width * .2,this.scale.height * .5,'Poker',{fontFamily: 'Ultra',fontSize: '2em',color: 'white'});
+        const pokerButton = this.add.text(this.scale.width * .2,this.scale.height * .5,'Poker',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
         pokerButton.setInteractive();
         pokerButton.on('pointerdown', () => {
+            loungeMusic.stop();
             this.scene.start('Poker');
+        });
+
+        pokerButton.on('pointerover', () => {
+            pokerButton.setColor('red');
+        });
+
+        pokerButton.on('pointerout', () => {
+            pokerButton.setColor('white');
         });
 
         // // Settings Button
@@ -100,10 +154,49 @@ class Roulette extends ConfigureScene {
         const rouletteText = this.add.text(this.scale.width * .4,this.scale.height * .05,'Roulette',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
 
     }
+    update(){
+
+    }
 
 }
 
 class Blackjack extends ConfigureScene {
+    constructor() {
+        super('Blackjack');
+        this.deck = [];
+        this.cardSprites = {};
+        this.suits = ['Clubs','Diamond','Hearts','Spades'];
+        this.ranks = [1,2,3,4,5,6,7,8,9,10,'J','Q','K'];
+
+    }
+    create(){
+        this.add.text(this.scale.width * .4,this.scale.height * .05,'Blackjack',{fontFamily: 'Ultra',fontSize: '4em',color: 'white'});
+
+        var ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+        // concat ranks 4 times to create a deck of cards 52 cards
+        for (var i = 0; i < 4; i++) {
+            this.deck = this.deck.concat(ranks);
+        }
+        console.log(this.deck);
+
+        // Deal 2 Cards to the player
+        var playerHand = [this.deck.splice(this.getRandomCardIndex(),1),this.deck.splice(this.getRandomCardIndex(),1)];
+        console.log(playerHand);
+        console.log(this.deck);
+
+        // Deal 2 Cards to the dealer
+        var dealerHand = [this.deck.splice(this.getRandomCardIndex(),1),this.deck.splice(this.getRandomCardIndex(),1)];
+        console.log(dealerHand);
+        console.log(this.deck);
+
+
+    }
+    getRandomCardIndex(){
+        return Math.floor(Math.random() * this.deck.length);
+    }
+    update(){}
+
 
 }
 
@@ -115,6 +208,8 @@ class Poker extends ConfigureScene {
 
 }
 
+class Settings extends ConfigureScene {}
+
 
 var config = {
     type: Phaser.AUTO,
@@ -125,7 +220,7 @@ var config = {
         width: '100%',  // Set the width of the game to 100% of the parent container
         height: '100%'  // Set the height of the game to 100% of the parent container
     },
-    scene: [ConfigureScene,Lounge,Roulette]  // Add your scene to the configuration
+    scene: [ConfigureScene,Lounge,Roulette,Blackjack]  // Add your scene to the configuration
 };
 
 var game = new Phaser.Game(config);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # luxelagoon-gaming-lounge
+
+ ## Attributions
+ "Music by Tri-Tachyon - https://soundcloud.com/tri-tachyon/albums".
+ 
